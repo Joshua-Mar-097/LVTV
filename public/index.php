57e9d56585df6fbe7b02b8b88fa2e1ca5a5f7ee6
Mar <joshuajamesmar@student.laverdad.edu.ph>
2024-04-14 16:51:13 +0800

    Added Log in and Log out components (not working) and Branch

--- public/index.php
+++ public/index.php
@@ -15,3 +15,6 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
