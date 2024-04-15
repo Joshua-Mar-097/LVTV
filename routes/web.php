c709bcd19ed086260dd347cca7bb09f0c37305cc
Mar <joshuajamesmar@student.laverdad.edu.ph>
2024-04-15 22:01:34 +0800

    Removed routes from web.php moved to api.php

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
 
-Route::get('/proxy/code', function (\Illuminate\Http\Request $request) {
-    try {
-        $response = Http::get('https://adgard.net/code', [
-            'id' => $request->query('id'),
-            'type' => $request->query('type'),
-        ]);
-
-        return $response->json();
-    } catch (\Exception $e) {
-        \Log::error('Proxy Error: ' . $e->getMessage());
-
-        return response()->json(['error' => $e->getMessage()], $e->getCode() ?: 500);
-    }
-});
-
 Route::get('/', function () {
     return view('welcome');
 });
